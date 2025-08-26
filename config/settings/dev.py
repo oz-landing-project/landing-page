6e3906547d0a2c803386e6f29c9b6b60301d8116
hoochucat <ohnanabanha@gmail.com>
2025-08-26 17:43:25 +0900

    ✨ 1일차 시작! :)
    앱 구조 설정

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -4,3 +4,17 @@ from .base import *
 
 DEBUG = True
 ALLOWED_HOSTS = []
+
+
+# config/settings/dev.py
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',  # 필수: PostgreSQL 사용
+        'NAME': 'landing_project_db',               # 데이터베이스 이름
+        'USER': 'postgres',                         # 사용자
+        'PASSWORD': '@qwer@1',                     # 비밀번호
+        'HOST': '127.0.0.1',                        # 또는 'localhost'
+        'PORT': '5432',                             # Postgres 포트
+    }
+}
